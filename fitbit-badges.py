c3c11e49aee63f9aabdce79a270e768af7b18f13
primate-2016 <richardwhit@sky.com>
2019-02-24 11:18:07 +0000

    initial

---
+++ fitbit-badges.py
@@ -0,0 +1,25 @@
+class FitBitBadgesUser():
+
+    def __init__(self):
+        pass
+
+    def get_user(self):
+        pass
+
+    def create_user(self):
+        pass
+
+class BadgesList():
+
+    def __init__(self):
+        pass
+    
+    def read_existing_badges(self):
+        pass
+    
+    def find_new_badges(self):
+        pass
+    
+    def write_new_badges(self):
+        pass
+    
